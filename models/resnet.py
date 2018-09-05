fb5e2a2a47bb07c64b04156a17685bf37b86604f
Tianz <ssstz@outlook.com>
2018-09-05 16:38:39 +0800

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -14,8 +14,8 @@ def residual_block(x, nb_filters, strides=(1, 1), weight_decay=1E-4, sparse_fact
                 strides=strides,
                 use_bias=False,
                 kernel_regularizer=l2(weight_decay))(x)
+    x = BatchNormalization()(x)
     x = SparsityRegularization(l1=sparse_factor)(x)
-    x = BatchNormalization(scale=False)(x)
     x = Activation('relu')(x)
     x = Conv2D(nb_filters, (3, 3),
                 kernel_initializer='he_normal',
